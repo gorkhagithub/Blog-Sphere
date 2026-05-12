f3761795d5c058ada7b180df5d769dbf7dd8e705
gorkhagithub <gorkhaabhishek1@gmail.com>
2026-05-12 09:11:37 +0530

    fixed axios production api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,16 +1,42 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+// });
+
+// // Request interceptor for adding the bearer token
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
+
+
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:5000/api',
 });
 
 // Request interceptor for adding the bearer token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => {
